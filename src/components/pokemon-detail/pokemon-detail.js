a635434659c7805af3de7239c7d6e81d27ecb508
FRAN\fmdia <fmdiazmontes87@gmail.com>
2024-04-12 08:24:00 +0200

    feat: pokemon-detail componente

---
+++ src/components/pokemon-detail/pokemon-detail.js
@@ -0,0 +1,37 @@
+import { LitElement, html, css, property } from 'lit';
+
+class PokemonDetail extends LitElement {
+  static styles = css`
+    .pokemon-container {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      padding: 16px;
+      border: 1px solid #ccc;
+      border-radius: 8px;
+      margin: 16px;
+    }
+
+    .pokemon-imagen {
+      width: 200px;
+      height: 200px;
+      object-fit: contain;
+    }
+  `;
+
+  @property({ type: Object }) pokemon;
+
+  render() {
+    return html`
+      <div class="pokemon-container">
+        <h2>${this.pokemon?.name}</h2>
+        <img class="pokemon-imagen" src=${this.pokemon?.sprites?.front_default} alt=${this.pokemon?.name}>
+        <p>Tipo: ${this.pokemon?.types?.map(typeInfo => typeInfo.type.name).join(', ')}</p>
+        <p>Altura: ${this.pokemon?.height}</p>
+        <p>Peso: ${this.pokemon?.weight}</p>
+      </div>
+    `;
+  }
+}
+
+customElements.define('pokemon-detail', PokemonDetail);
